27e5e9b712403b5e05cc0600a20df029b3713f3c
quileab <quileab@gmail.com>
2021-12-28 10:45:31 -0300

    restored test

--- app/Models/Warehouse.php
+++ app/Models/Warehouse.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Warehouse extends Model
 {
     use HasFactory;
+    // fillable
+    protected $fillable = [
+        'name',
+        'location',
+        'phone',
+        'email',
+        'contact_person',
+    ];
 }
